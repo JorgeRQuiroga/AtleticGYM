260e7e4605baac73c3a3185602839249509bbc6f
JorgeRQuiroga <jorge.quiroga2410@gmail.com>
2025-11-27 21:36:43 -0300

    modificaciones a caja

--- cajas/forms.py
+++ cajas/forms.py
@@ -1,10 +1,22 @@
 from django import forms
 from .models import Caja
+from .models import Caja, ConfiguracionCaja
 
 class AperturaCajaForm(forms.ModelForm):
     class Meta:
         model = Caja
-        fields = ['monto_apertura']
+        fields = []
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        config = ConfiguracionCaja.objects.first()
+        if config:
+            instance.monto_apertura = config.monto_inicial
+        if commit:
+            instance.save()
+        return instance
+
+
 
 class CierreCajaForm(forms.ModelForm):
     class Meta:
--- login/urls.py
+++ login/urls.py
@@ -5,4 +5,4 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-]
+    ]
